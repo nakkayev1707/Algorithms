dd540c6db546a8143fc2477308fa14e93d1178b3
Ismail <nakkazulav@mail.ru>
2024-01-02 02:00:37 +0400

    remove unnecessary files

---
+++ Algorithms/Leetcode/Arrays/Task6/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace Algorithms.Leetcode.Arrays.Task6;
+
+public class Solution
+{
+    public static int MaxProduct(int[] nums)
+    {
+        
+        // TODO: Finish implementation, return to this task in Leetcode
+        int product = 0;
+        var maxProduct = nums[0];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            product *= nums[i];
+            
+            if (nums[i] > product)
+            {
+                product = nums[i];
+            }
+
+            if (product > maxProduct)
+            {
+                maxProduct = product;
+            }
+        }
+
+        return maxProduct;
+    }
+}
