71c4c7483082e266de4b3eadddb53d9fc526100c
Ismail <nakkazulav@mail.ru>
2024-01-02 02:00:04 +0400

    ignore unnecessary files

--- Algorithms/Leetcode/Arrays/Task3/Solution.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Algorithms.Leetcode.Arrays.Task3;
-
-public class Solution
-{
-    public static bool ContainsDuplicate(int[] nums)
-    {
-        return new HashSet<int>(nums).Count != nums.Length;
-    }
-}
