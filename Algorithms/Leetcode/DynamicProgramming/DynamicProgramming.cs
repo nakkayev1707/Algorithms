860aeb3cb926d8aedb716eb832a0c441083e9cef
Ismail <nakkazulav@mail.ru>
2024-06-06 22:10:25 +0400

    DynamicProgramming: ConiChange - in progress

--- Algorithms/Leetcode/DynamicProgramming/DynamicProgramming.cs
+++ Algorithms/Leetcode/DynamicProgramming/DynamicProgramming.cs
@@ -5,11 +5,18 @@ public class DynamicProgramming
     public static void Run()
     {
         Task1();
+        Task2();
     }
     
     private static void Task1()
     {
         int result = Algorithms.Leetcode.DynamicProgramming.Task1.Solution.ClimbStairs(3);
+        Console.WriteLine(result);
+    }
+    
+    private static void Task2()
+    {
+        int result = Algorithms.Leetcode.DynamicProgramming.Task2.Solution.CoinChange(new []{1,2,5}, 11);
         Console.WriteLine(result);
     }
 }
---
+++ Algorithms/Leetcode/DynamicProgramming/Task2/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace Algorithms.Leetcode.DynamicProgramming.Task2;
+
+public class Solution
+{
+    public static int CoinChange(int[] coins, int amount)
+    {
+        int max = coins[0];
+        for (int i = 0; i < coins.Length; i++)
+        {
+            if (coins[i] > max)
+            {
+                max = coins[i];
+            }
+        }
+
+        if (max < amount) return -1;
+
+        int result = 0;
+        int sum = 0;
+        while (sum != amount)
+        {
+            
+        }
+
+        return result;
+    }
+}
