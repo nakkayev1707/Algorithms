eb95246001016ffa414f9b0cf2ed1896de81bff2
Ismail <nakkazulav@mail.ru>
2024-06-06 21:44:17 +0400

    DynamicProgramming: ClimbStairs]

--- Algorithms/Leetcode/DynamicProgramming/Task1/Solution.cs
+++ Algorithms/Leetcode/DynamicProgramming/Task1/Solution.cs
@@ -4,15 +4,17 @@ public class Solution
 {
     public static int ClimbStairs(int n)
     {
-        int result = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (i % 1 == 0 || i % 2 == 0)
-            {
-                result++;
-            }
-        }
+        if (n <= 1) return 1;
+        
+        int[] res = new int[n + 1];
+        res[0] = 1;
+        res[1] = 1;
 
-        return result;
+        for (int i = 2; i <= n; i++)
+        {
+            res[i] = res[i - 1] + res[i - 2];
+        }
+        
+        return res[n];
     }
 }
