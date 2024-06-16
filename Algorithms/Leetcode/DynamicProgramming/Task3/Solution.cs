1535fd92f4a5fff3671116cec09e4cddd8970d16
Ismail <nakkazulav@mail.ru>
2024-06-17 00:56:55 +0400

    DynamicProgramming Task3 - in progress

--- Algorithms/Leetcode/DynamicProgramming/Task3/Solution.cs
+++ Algorithms/Leetcode/DynamicProgramming/Task3/Solution.cs
@@ -2,7 +2,20 @@
 
 public static class Solution
 {
-    public static int LengthOfLIS(int[] nums) {
+    // Input: nums = [10,9,2,5,3,7,101,18]
+    // Output: 4
+    public static int LengthOfLIS(int[] nums)
+    {
+        if (nums == null || nums.Length == 0) return 0;
+
+        int n = nums.Length;
+        int[] dp = new int[n];
         
+        Array.Fill(dp, 1);
+
+        for ()
+        {
+            // TODO: continue
+        }
     }
 }
