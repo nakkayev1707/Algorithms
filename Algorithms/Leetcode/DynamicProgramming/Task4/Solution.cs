4771562e007498cc3f5eec481e95fe7a1970ed43
Ismail <nakkazulav@mail.ru>
2025-01-29 00:08:20 +0400

    Task4 DP: resolved

--- Algorithms/Leetcode/DynamicProgramming/Task4/Solution.cs
+++ Algorithms/Leetcode/DynamicProgramming/Task4/Solution.cs
@@ -9,19 +9,42 @@ public class Solution
         int len1 = text1.Length;
         int len2 = text2.Length;
 
-        return LCS(text1, text2, len1, len2);
-    }
+        if (len1 == 0 || len2 == 0)
+        {
+            return 0;
+        }
 
+        var dp = new int[len1 + 1, len2 + 1];
+
+        for (int i = 1; i <= len1; i++)
+        {
+            for (int j = 1; j <= len2; j++)
+            {
+                if (text1[i - 1] == text2[j - 1])
+                {
+                    dp[i, j] = 1 + dp[i - 1, j - 1];
+                }
+                else
+                {
+                    dp[i, j] = Math.Max(dp[i, j - 1], dp[i - 1, j]);
+                }
+            }
+        }
+
+        return dp[len1, len2];
+    }
+    
+    /// solution using recursion
+    /// return LCS(text1, text2, len1, len2);
     private static int LCS(string str1, string str2, int len1, int len2)
     {
         if (len1 == 0 || len2 == 0) return 0;
-
+    
         if (str1[len1 - 1] == str2[len2 - 1])
         {
             return 1 + LCS(str1, str2, len1 - 1, len2 - 1);
         }
-
+    
         return Math.Max(LCS(str1, str2, len1, len2 -1), LCS(str1, str2, len1 - 1, len2));
     }
-    
 }
