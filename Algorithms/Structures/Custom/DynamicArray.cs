0c08d6c1d7ecb696d0ef18728755a109bb34555a
Ismail <nakkazulav@mail.ru>
2025-03-01 15:53:52 +0400

    dynamicArray

---
+++ Algorithms/Structures/Custom/DynamicArray.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace Algorithms.Structures.Custom;
+
+public class DynamicArray
+{
+    private int[] arr;
+    
+    public DynamicArray(int capacity)
+    {
+        Assert.True(capacity > 0);
+        arr = new int[capacity];
+    }
+
+    public int Get(int i)
+    {
+        Assert.True(i >= 0);
+        return arr[i];
+    }
+
+    public void Set(int i, int n)
+    {
+        Assert.True(i >= 0);
+        arr[i] = n;
+    }
+
+    public void PushBack(int n)
+    {
+        
+    }
+
+    public int PopBack()
+    {
+    }
+
+    private void Resize()
+    {
+    }
+
+    public int GetSize()
+    {
+    }
+
+    public int GetCapacity()
+    {
+    }
+}
